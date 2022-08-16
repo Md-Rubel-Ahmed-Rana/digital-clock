a1226e976280b24202c3b7277269ba5ae387b35d
Md Rubel Ahmed Rana <mdrubelahmedrana521@gmail.com>
2022-08-16 16:03:54 +0600

    Bugs fixed

--- main.js
+++ main.js
@@ -1,35 +1,37 @@
-;
 const getHour = document.getElementById("hour");
 const getMinute = document.getElementById("minute");
 const getSecond = document.getElementById("second");
-const timeFormat = document.getElementById("time-format");
+let timeFormat = document.getElementById("time-format");
+
 
 function time(){
     const date = new Date();
     let hours = date.getHours();
     let minutes = date.getMinutes();
     let seconds = date.getSeconds();
-    const getTime = hours + " " + minutes + " " + seconds;
-    getHour.innerHTML = hours;
-    getMinute.innerHTML = minutes;
-    getSecond.innerHTML = seconds;
 
-    if ( seconds < 10) {
-        getSecond.innerHTML = "0" + seconds;
+    if (hours > 12) {
+      hours = hours - 12;
+      timeFormat.innerText = "PM";
+    } else{
+      hours = hours
     }
 
-    if (minutes < 10) {
-        getMinute.innerHTML = "0" + minutes;
+    if (hours < 10 ) {
+      hours = "0" + hours
+    }
+    
+    if (minutes < 10 ) {
+      minutes = "0" + minutes
     }
 
-    if(hours < 10) {
-        getHour.innerHTML = "0" + hours;
-    } else if(hours > 12){
-        hours = hours - 12;
-        getHour.innerHTML = hours;
-        timeFormat.innerHTML = "PM";
+    if (seconds < 10 ) {
+      seconds = "0" + seconds
     }
-    return getTime
+    
+    getHour.innerText = hours;
+    getMinute.innerText = minutes;
+    getSecond.innerText = seconds;
 }
 setInterval(() => {
     time();
